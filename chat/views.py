b8ef91e84d798351d985e377dcb4c57596662384
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2022-06-13 13:58:08 +0200

    create post method

--- chat/views.py
+++ chat/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST':
+        print(request.POST)
     return render(request, 'chat/index.html', {'username': 'Testuser'})
