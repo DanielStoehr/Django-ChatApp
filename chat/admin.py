47b04407890a05fa7a386ddf1b498423a1696b27
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2022-06-13 13:37:25 +0200

    edit admin area

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
 
 # Register your models here.
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'author', 'text', 'receiver')
+
+
+admin.site.register(Message, MessageAdmin)
